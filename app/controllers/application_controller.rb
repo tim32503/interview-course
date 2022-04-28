2d77daac029635f527a72f4051b3baca1d2f9877
Tim Hsueh <timhsueh@curihaosity.xyz>
2022-04-28 22:24:35 +0800

    feat: 新增管理後台 #9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
   protected
 
   def configure_permitted_parameters
